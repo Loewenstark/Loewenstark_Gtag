f1b4bd5b4bf4839cfe9ccce50b3c2e53464ad7a1
Mathis Klooß <mathis@mage-profis.de>
2019-05-09 08:31:24 +0200

    [~Fixed] Crashed in Magento 1.9.0.1
    Call to undefined method Loewenstark_Gtag_Helper_Data::getAccountId()

--- src/code/Helper/Data.php
+++ src/code/Helper/Data.php
@@ -3,5 +3,17 @@
 class Loewenstark_Gtag_Helper_Data
 extends Mage_GoogleAnalytics_Helper_Data
 {
-    
+
+    const XML_PATH_ACCOUNT       = 'google/analytics/account';
+
+    /**
+     * get Account Id
+     * 
+     * @param mixed $store
+     * @return string
+     */
+    public function getAccountId($store = null)
+    {
+        return Mage::getStoreConfig(self::XML_PATH_ACCOUNT, $store);
+    }
 }
